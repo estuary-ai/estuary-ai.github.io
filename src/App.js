a0785f16052805b24e7d313dfa5e187879fc6ade
lemonlemonde <mirudjun@gmail.com>
2024-07-31 04:08:41 -0700

    :construction: Move routing from App.js to index.js
    where it should be

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
 
-import { Routes, Route } from "react-router-dom";
-
-import Layout from "./components/BaseLayout";
-import HomePage from "./pages/HomePage";
-import DocsPage from "./pages/DocsPage";
-import DemosPage from "./pages/DemosPage";
-import NoPage from "./pages/NoPage";
 
 export default function App() {
-  // TODO: reference this if having redirect or routing issues!!! (also possibly for pages loading in the middle)
-    // need to do scrollToTop() in the Layout component or smth
-    // https://stackoverflow.com/questions/73364038/github-pages-is-not-loading-subpage-even-though-url-is-correct
-    // more explanation here: https://github.com/orgs/community/discussions/36908
+  
   return (
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="docs" element={<DocsPage />} />
-            <Route path="demos" element={<DemosPage />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </React.StrictMode>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Layout />}>
-    //       <Route index element={<HomePage />} />
-    //       <Route path="docs" element={<DocsPage />} />
-    //       <Route path="demos" element={<DemosPage />} />
-    //       <Route path="*" element={<NoPage />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
+    <h1>App</h1>
   );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { HashRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/BaseLayout";
+import HomePage from "./pages/HomePage";
+import DocsPage from "./pages/DocsPage";
+import DemosPage from "./pages/DemosPage";
+import NoPage from "./pages/NoPage";
+
+// TODO: reference this if having redirect or routing issues!!! (also possibly for pages loading in the middle)
+    // need to do scrollToTop() in the Layout component or smth
+    // https://stackoverflow.com/questions/73364038/github-pages-is-not-loading-subpage-even-though-url-is-correct
+    // more explanation here: https://github.com/orgs/community/discussions/36908
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="docs" element={<DocsPage />} />
+            <Route path="demos" element={<DemosPage />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </Router>
   </React.StrictMode>
 );
 
