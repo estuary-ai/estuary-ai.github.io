b6b860e86e6450b6b94a7091e8b910f61857dbca
lemonlemonde <mirudjun@gmail.com>
2024-07-31 03:41:23 -0700

    :sparkles: Table of Contents done with proper offset scrolling

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,152 +1,178 @@
 import '../css/TableOfContents.css';
+import React from 'react';
 
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 
 // finds and makes headings and makes scroll into view
-const Headings = ({ headings, activeId }) => {
-    <ul>
-        {headings.map(heading => (
-            <li key={heading.id} className={heading.id === activeId ? 'active' : ''}>
-                <a 
-                    href={`#${heading.id}`} 
-                    onClick={(e) => {
-                        e.preventDefault();
-                        document.querySelector(`#${heading.id}`).scrollIntoView({
-                            behavior: 'smooth'
-                        });
-                    }}
+const Headings = ({ headings, activeId }) => (
+  <ul>
+    {headings.map((heading) => (
+      <li key={heading.id} className={heading.id === activeId ? "active" : ""}>
+        <a
+          href={`#${heading.id}`}
+          onClick={(e) => {
+            e.preventDefault();
+            // const el = document.querySelector(`#${heading.id}`);
+            // const yOffset = -10;
+            // const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
+            // window.scrollTo({top: y, behavior: 'smooth'});
+            document.querySelector(`#${heading.id}`).scrollIntoView({
+                block: "start",
+                behavior: "smooth"
+            });
+          }}
+        >
+          {heading.title}
+        </a>
+        {heading.items.length > 0 && (
+          <ul>
+            {heading.items.map((child) => (
+              <li
+                key={child.id}
+                className={child.id === activeId ? "active" : ""}
+              >
+                <a
+                  href={`#${child.id}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    // const el = document.querySelector(`#${child.id}`);
+                    // const yOffset = -10;
+                    // const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
+                    // window.scrollTo({top: y, behavior: 'smooth'});
+                    document.querySelector(`#${child.id}`).scrollIntoView({
+                        block: "start",
+                        behavior: "smooth"
+                    });
+                  }}
                 >
-                    {heading.title}
+                  {child.title}
                 </a>
-                {heading.items.length > 0 && (
-                    <ul>
-                        {heading.items.map((child) => (
-                            <li
-                                key={child.id}
-                                className={child.id === activeId ? "active" : ""}
-                            >
-                                <a
-                                    href={`#${child.id}`}
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        document.querySelector(`#${child.id}`).scrollIntoView({
-                                            behavior: "smooth"
-                                        });
-                                    }}
-                                >
-                                    {child.title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </li>
-        ))}
-    </ul>
-}
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+    ))}
+  </ul>
+);
 
+/**
+ * Dynamically generates the table of contents list, using any H2s and H3s it can find in the main text
+ */
 const useHeadingsData = () => {
-    const [nestedHeadings, setNestedHeadings] = useState([]);
+  const [nestedHeadings, setNestedHeadings] = React.useState([]);
 
-    useEffect(() => {
-        // get all headings
-        const headingElements = Array.from(document.querySelectorAll("main h2, main h3"));
+  React.useEffect(() => {
+    const headingElements = Array.from(
+      document.querySelectorAll("main h2, main h3")
+    );
 
-        // make and set heading hierarchy
-        const newNestedHeadings = getNestedHeadings(headingElements);
-        setNestedHeadings(newNestedHeadings);
-    }, []);
+    // Created a list of headings, with H3s nested
+    const newNestedHeadings = getNestedHeadings(headingElements);
+    setNestedHeadings(newNestedHeadings);
+  }, []);
 
-    return { nestedHeadings };
-}
+  return { nestedHeadings };
+};
 
-// go through all found headings and categorize into a hierarchy
 const getNestedHeadings = (headingElements) => {
-    const nestedHeadings = [];
+  const nestedHeadings = [];
 
-    headingElements.forEach((heading, index) => {
-        const { innerText: title, id} = heading;
+  headingElements.forEach((heading, index) => {
+    const { innerText: title, id } = heading;
 
-        if (heading.nodeName === "H2") {
-            // if it's a section heading, it has subsections (items)
-            nestedHeadings.push({ title, id, items: [] });
-        } else if (heading.nodeName === "H3" && nestedHeadings.length > 0) {
-            // if it's a subsection heading, it's a leaf node in the hierarchy
-            nestedHeadings[nestedHeadings.length - 1].items.push({ title, id });
-        }
-    });
+    if (heading.nodeName === "H2") {
+      nestedHeadings.push({ id, title, items: [] });
+    } else if (heading.nodeName === "H3" && nestedHeadings.length > 0) {
+      nestedHeadings[nestedHeadings.length - 1].items.push({
+        id,
+        title
+      });
+    }
+  });
 
-    return nestedHeadings;
-}
+  return nestedHeadings;
+};
 
 const useIntersectionObserver = (setActiveId) => {
-    const headingElementsRef = useRef([]);
-    useEffect(() => {
-        const callback = (headings) => {
-            headingElementsRef.current = headings.reduce((map, headingElement) => {
-                map[headingElement.target.id] = headingElement;
-                return map;
-            }, headingElementsRef.current);
+  const headingElementsRef = React.useRef({});
+  React.useEffect(() => {
+    const callback = (headings) => {
+      headingElementsRef.current = headings.reduce((map, headingElement) => {
+        map[headingElement.target.id] = headingElement;
+        return map;
+      }, headingElementsRef.current);
 
-            const visibleHeadings = [];
-            Object.keys(headingElementsRef.current).forEach(key => {
-                const headingElement = headingElementsRef.current[key];
-                if (headingElement.isIntersecting) {
-                    visibleHeadings.push(headingElement);
-                }
-            });
+      // Get all headings that are currently visible on the page
+      const visibleHeadings = [];
+      Object.keys(headingElementsRef.current).forEach((key) => {
+        const headingElement = headingElementsRef.current[key];
+        if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
+      });
 
-            const getIndexFromId = (id) => {
-                return headingElementsRef.findIndex((heading) => heading.id === id);
-            }
+      const getIndexFromId = (id) =>
+        headingElements.findIndex((heading) => heading.id === id);
 
-            if (visibleHeadings.length === 1) {
-                setActiveId(visibleHeadings[0].target.id);
-            } else if (visibleHeadings.length > 1) {
-                const sortedVisibleHeadings = visibleHeadings.sort((a, b) => {
-                    return getIndexFromId(a.target.id) > getIndexFromId(b.target.id);
-                });
+      // If there is only one visible heading, this is our "active" heading
+      if (visibleHeadings.length === 1) {
+        setActiveId(visibleHeadings[0].target.id);
+        // If there is more than one visible heading,
+        // choose the one that is closest to the top of the page
+      } else if (visibleHeadings.length > 1) {
+        const sortedVisibleHeadings = visibleHeadings.sort(
+          (a, b) => getIndexFromId(a.target.id) > getIndexFromId(b.target.id)
+        );
 
-                setActiveId(sortedVisibleHeadings[0].target.id);
-            }
-        };
+        setActiveId(sortedVisibleHeadings[0].target.id);
+      }
+    };
 
-        const observer = new IntersectionObserver(callback, {
-            root: document.querySelector("iframe"), 
-            rootMargin: "500px"
-        });
+    const observer = new IntersectionObserver(callback, { root: document.querySelector("iframe"), rootMargin: "500px" });
 
+    const headingElements = Array.from(document.querySelectorAll("h2, h3"));
 
-        const headingElements = Array.from(document.querySelectorAll("h2, h3"));
+    headingElements.forEach((element) => observer.observe(element));
 
-        headingElements.forEach((element) => {
-            observer.observe(element);
-        });
+    return () => observer.disconnect();
+  }, [setActiveId]);
+};
 
-        return () => {
-            observer.disconnect();
-        }
-    }, [setActiveId]);
-}
+/**
+ * Renders the table of contents.
+ */
+// const TableOfContents = () => {
+//   const [activeId, setActiveId] = React.useState();
+//   const { nestedHeadings } = useHeadingsData();
+//   useIntersectionObserver(setActiveId);
+
+//   return (
+//     <nav aria-label="Table of contents">
+//       <Headings headings={nestedHeadings} activeId={activeId} />
+//     </nav>
+//   );
+// };
 function TableOfContents() {
     const [activeId, setActiveId] = useState(null);
     const { nestedHeadings } = useHeadingsData();
     useIntersectionObserver(setActiveId);
 
     return (
-        <nav aria-label="Table of contents" className='toc'>
+        <div className='toc'>
             <h2>Table of Contents</h2>
-            <Headings headings={nestedHeadings} activeId={activeId} />
-            
+            <nav aria-label="Table of contents">
+                <Headings headings={nestedHeadings} activeId={activeId} />
+            </nav>
+
             <style js>{`
-                {/* when screen width small */}
-                @media (max-width: 767px){
-                    .toc{
-                        display: none;
+                    {/* when screen width small */}
+                    @media (max-width: 767px){
+                        .toc{
+                            display: none;
+                        }
                     }
-                }
             `}</style>
-        </nav>
+
+        </div>
         
     )
 }
--- src/pages/DocsPage.js
+++ src/pages/DocsPage.js
@@ -2,24 +2,37 @@
 
 import '../css/DocsPage.css';
 
-// import { Card } from 'react-bootstrap';
 import TableOfContents from '../components/TableOfContents';
-// import { useRef } from 'react';
-// import { computeHeadingLevel } from '@testing-library/react';
 
 function DocsPage() {
     return (
-        <div className='sidebyside'>
+        <div className='container'>
             <main className='mainContent'>
                 <h1 id="title">Documentation for Estuary</h1>
 
                 
-                <h2 id="disclaimer">Documentation to be added soon!</h2>
+                <h2 id="disclaimer">Introduction</h2>
                 <p>
                 Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus vehicula congue nibh interdum; consequat consectetur suscipit mollis. Aplacerat felis dignissim tristique vestibulum sollicitudin. Enim ridiculus per tristique augue morbi magna. Fringilla bibendum cras scelerisque dis dictumst inceptos senectus malesuada. Luctus vitae justo mattis porttitor praesent porttitor elit amet! Integer platea tempus platea elementum ullamcorper faucibus.
 
                 Vitae inceptos conubia cursus habitasse proin habitant pharetra. Ad auctor hendrerit primis tempus; nibh bibendum. Taciti morbi nullam magnis feugiat vivamus. Dis at maecenas fames erat condimentum risus sed. Purus facilisi ipsum vestibulum maximus nostra et sit lectus. Massa nam mus pellentesque; aptent donec habitasse maximus. Nostra lobortis dolor pellentesque finibus vehicula, felis suscipit fermentum. Arcu volutpat tortor class vitae faucibus primis condimentum elementum.
                 </p>
+                <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus vehicula congue nibh interdum; consequat consectetur suscipit mollis. Aplacerat felis dignissim tristique vestibulum sollicitudin. Enim ridiculus per tristique augue morbi magna. Fringilla bibendum cras scelerisque dis dictumst inceptos senectus malesuada. Luctus vitae justo mattis porttitor praesent porttitor elit amet! Integer platea tempus platea elementum ullamcorper faucibus.
+
+                Vitae inceptos conubia cursus habitasse proin habitant pharetra. Ad auctor hendrerit primis tempus; nibh bibendum. Taciti morbi nullam magnis feugiat vivamus. Dis at maecenas fames erat condimentum risus sed. Purus facilisi ipsum vestibulum maximus nostra et sit lectus. Massa nam mus pellentesque; aptent donec habitasse maximus. Nostra lobortis dolor pellentesque finibus vehicula, felis suscipit fermentum. Arcu volutpat tortor class vitae faucibus primis condimentum elementum.
+                </p>
+                <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus vehicula congue nibh interdum; consequat consectetur suscipit mollis. Aplacerat felis dignissim tristique vestibulum sollicitudin. Enim ridiculus per tristique augue morbi magna. Fringilla bibendum cras scelerisque dis dictumst inceptos senectus malesuada. Luctus vitae justo mattis porttitor praesent porttitor elit amet! Integer platea tempus platea elementum ullamcorper faucibus.
+
+                Vitae inceptos conubia cursus habitasse proin habitant pharetra. Ad auctor hendrerit primis tempus; nibh bibendum. Taciti morbi nullam magnis feugiat vivamus. Dis at maecenas fames erat condimentum risus sed. Purus facilisi ipsum vestibulum maximus nostra et sit lectus. Massa nam mus pellentesque; aptent donec habitasse maximus. Nostra lobortis dolor pellentesque finibus vehicula, felis suscipit fermentum. Arcu volutpat tortor class vitae faucibus primis condimentum elementum.
+                </p>
+                <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus vehicula congue nibh interdum; consequat consectetur suscipit mollis. Aplacerat felis dignissim tristique vestibulum sollicitudin. Enim ridiculus per tristique augue morbi magna. Fringilla bibendum cras scelerisque dis dictumst inceptos senectus malesuada. Luctus vitae justo mattis porttitor praesent porttitor elit amet! Integer platea tempus platea elementum ullamcorper faucibus.
+
+                Vitae inceptos conubia cursus habitasse proin habitant pharetra. Ad auctor hendrerit primis tempus; nibh bibendum. Taciti morbi nullam magnis feugiat vivamus. Dis at maecenas fames erat condimentum risus sed. Purus facilisi ipsum vestibulum maximus nostra et sit lectus. Massa nam mus pellentesque; aptent donec habitasse maximus. Nostra lobortis dolor pellentesque finibus vehicula, felis suscipit fermentum. Arcu volutpat tortor class vitae faucibus primis condimentum elementum.
+                </p>
+                <h3 id="something">Something</h3>
                 <p>
                 Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus vehicula congue nibh interdum; consequat consectetur suscipit mollis. Aplacerat felis dignissim tristique vestibulum sollicitudin. Enim ridiculus per tristique augue morbi magna. Fringilla bibendum cras scelerisque dis dictumst inceptos senectus malesuada. Luctus vitae justo mattis porttitor praesent porttitor elit amet! Integer platea tempus platea elementum ullamcorper faucibus.
 
