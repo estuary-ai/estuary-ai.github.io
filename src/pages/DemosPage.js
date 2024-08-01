61355811b0e7dcebb47818a01813af43ba805b1e
lemonlemonde <mirudjun@gmail.com>
2024-08-01 02:37:46 -0700

    Add placeholder yt vid for demos

--- src/pages/DemosPage.js
+++ src/pages/DemosPage.js
@@ -12,6 +12,17 @@ function DemosPage() {
                     </Card.Text>
                 </Card.Body>
             </Card>
+
+            <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/VTVzmlEPGPM?si=knQUnD5_-rnj52Jf" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe>
         </div>
 
     )
