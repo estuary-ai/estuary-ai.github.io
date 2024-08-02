b616aa6c3e11a87d4db2816a0c673cdee1cd0934
lemonlemonde <mirudjun@gmail.com>
2024-08-01 23:36:33 -0700

    :bug: Fix overscroll behavior

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,15 +3,10 @@ import { Link } from "react-router-dom";
 
 function SideBar() {
     return (
-        <div className='sidebarContainer'>
-            <div className="sidebarLinks">
-                <Link to="/docs/quickstart">Quick Start</Link>
-                <Link to="/docs/quickstart">Quick Start</Link>
-                <Link to="/docs/quickstart">Quick Start</Link>
-
-            </div>
-            <div className='background1' />
-            <div className='background2' />
+        <div className="sidebarContainer">
+            <Link to="/docs/quickstart">Quick Start</Link>
+            <Link to="/docs/quickstart">Quick Start</Link>
+            <Link to="/docs/quickstart">Quick Start</Link>
         </div>
     );
 }
