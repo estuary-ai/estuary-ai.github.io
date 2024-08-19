3916f5d9e740e46c4a66d6d732782bbb8067ccdb
lemonlemonde <mirudjun@gmail.com>
2024-08-19 14:34:06 -0700

    :construction: Prettify toc and sidebar

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -173,7 +173,6 @@ function TableOfContents() {
 
     return (
           <nav aria-label="Table of contents" className='toc'>
-              <h2>Table of Contents</h2>
               <Headings headings={nestedHeadings} activeId={activeId} />
           </nav>
       
