176333c7d9dea42f159a890c650b12939622ebca
lemonlemonde <mirudjun@gmail.com>
2024-09-12 21:30:15 -0700

    :sparkles: Add demo vid to DemosPage

--- src/pages/DemosPage.js
+++ src/pages/DemosPage.js
@@ -3,30 +3,23 @@ import Title from '../components/Title';
 import GetStarted from '../components/GetStarted';
 import backgroundVideo from "../assets/Title_Background.mp4";
 
-
 function DemosPage() {
     return (
         // style={{marginTop: "80px", marginLeft: "30px", marginRight: "30px"}}
-        <div>
-            {/* <Card style={{position: "inherit"}}>
-                <Card.Body>
-                    <Card.Title>Demos</Card.Title>
-                    <Card.Text>
-                        Demo videos and papers to be added soon!
-                    </Card.Text>
-                </Card.Body>
-            </Card> */}
+        <div className='demoEntire'>
             <Title mainTitle={"Demo"} subtitle={"Demo videos and papers to be added soon!"} bgv={backgroundVideo} showButtons={false}/>
-            <iframe 
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/VTVzmlEPGPM?si=knQUnD5_-rnj52Jf" 
-                title="YouTube video player" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerpolicy="strict-origin-when-cross-origin" 
-                allowfullscreen>
-            </iframe>
+            <div style={{textAlign: "center"}}>
+                <iframe
+                    className='demoVid'
+                    src="https://www.youtube.com/embed/m5wO7E0zet8" 
+                    title="IVA24 Longer Demo" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                </iframe>
+
+            </div>
             <GetStarted />
 
         </div>
