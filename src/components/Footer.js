1977ca0f601a412ad69cfa85238f07194cd0d9c4
Spencer Lin <linspenc@usc.edu>
2025-01-07 14:38:43 -0800

    🔗 Updated Discord link to be permanent and changed Arxiv link to ACM

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                     {/* left column */}
                     <div>
                         <p className="footerTitle">Resources</p>
-                        <a className="linkbtnFooter arxivLogo"  href="https://arxiv.org/abs/2410.20116" target="_blank" rel="noreferrer" />
+                        <a className="linkbtnFooter arxivLogo"  href="https://dl.acm.org/doi/pdf/10.1145/3652988.3696198" target="_blank" rel="noreferrer" />
                         <a className="linkbtnFooter githubLogo" href="https://github.com/estuary-ai" target="_blank" rel="noreferrer" />
                         <a className="linkbtnFooter youtubeLogo" href="https://youtu.be/m5wO7E0zet8" target="_blank" rel="noreferrer" />
 
@@ -20,7 +20,7 @@ function Footer() {
                     <div>
                         <p className='footerTitle'>Contact Us</p>
                         {/* Discord invite link */}
-                        <a className="linkbtnFooter discordLogo" href="https://discord.gg/2Nazr9w25n" target="_blank" rel="noreferrer" />
+                        <a className="linkbtnFooter discordLogo" href="https://discord.gg/r6GDWkzbM9" target="_blank" rel="noreferrer" />
                         {/* email */}
                         <a className="linkbtnFooter emailLogo" href="mailto:estuary.ai@gmail.com" target="_blank" rel="noreferrer" />
                         <p>estuary [dot] ai [at] gmail [dot] com</p>
--- src/components/Title.js
+++ src/components/Title.js
@@ -17,7 +17,7 @@ const Title = (props) => {
                     {
                         showButtons &&
                         <div className="buttonWrapper">
-                            <a className="linkbtnFooter arxivLogo" href="https://arxiv.org/abs/2410.20116" target="_blank" rel="noreferrer">
+                            <a className="linkbtnFooter arxivLogo" href="https://dl.acm.org/doi/pdf/10.1145/3652988.3696198" target="_blank" rel="noreferrer">
                                 {/* Paper */}
                             </a>
                             <a className="linkbtnFooter githubLogo" href="https://github.com/estuary-ai" target="_blank" rel="noreferrer">
